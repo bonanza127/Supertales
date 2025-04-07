b3ede0f80a63f7a3df20f997c05f3f9da6e5b40f
bonanza127 <87675030+bonanza127@users.noreply.github.com>
2025-04-08 08:34:30 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-```javascript
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // グローバルCSSをインポート
-import App from './App'; // Appコンポーネントをインポート
+import './index.css'; // CSSファイルのインポート
+import App from './App'; // Appコンポーネントのインポート
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-```
+// public/index.html の <div id="root"></div> を取得
+const rootElement = document.getElementById('root');
+if (rootElement) {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+} else {
+    console.error('Root element not found'); // ルート要素が見つからない場合のエラー
+}
